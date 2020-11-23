d9c9d32c56bc0619e692e3974d2ce242425ff5e7
rossgeo <rossg@live.ie>
2020-11-23 13:15:26 +0000

    Setup Django, AllAuth setup, allauth templates and homepage/public app

--- bandz2/settings.py
+++ bandz2/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'allauth', #added
     'allauth.account', #added
     'allauth.socialaccount', #added
+    # Local Apps
+    'public',
     # Other
     'crispy_forms',
 ]
@@ -60,7 +62,10 @@ ROOT_URLCONF = 'bandz2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- bandz2/urls.py
+++ bandz2/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from public.views import HomepageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    # path("",
+    #     TemplateView.as_view(template_name="public_index.html"),
+    #     name="home",
+    # ),
+    path('', HomepageView.as_view(), name='home'),
 ]
