25f965a094e1f8401ed85dbdccfc19230a44ece0
rossgeo <rossg@live.ie>
2020-11-24 11:52:55 +0000

    Pre Postgres Install

--- public/models.py
+++ public/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from .sqlite_listfield import ListField
 
 # Create your models here.
+class County(models.Model):
+    name = models.CharField("County", max_length=55)
+    friendly_name = models.CharField(max_length=55, null=True, blank=True)
+
+    class Province(models.TextChoices):
+        ULSTER = "ulster", "Ulster"
+        LEINSTER = "leinster", "Leinster"
+        MUNSTER = "munster", "Munster"
+        CONNACHT = "connacht", "Connacht"
+
+    province = models.CharField("Province", max_length=10, choices=Province.choices, default=Province.LEINSTER)
+    towns = ListField(null=False)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
